0000765ffa08febc813de96f15fe649ed525487b
Chi-En Wu <jason2506.mail@gmail.com>
2013-06-10 15:20:56 +0800

    Change command-line argument format

--- bin/seed.sh
+++ bin/seed.sh
@@ -3,13 +3,11 @@
 . $( dirname "$0" )/config.cfg
 
 mkdir -p $data/seeds
-python $src/main.py seed \
-    --type  anew \
+python $src/main.py seed anew \
     --raw   $data/raw/anew.csv \
     --seed  $data/seeds/anew.tsv \
     --nodes $data/graph/nodes.tsv
-python $src/main.py seed \
-    --type  sn \
+python $src/main.py seed sn \
     --raw   $data/raw/senticnet.rdf.xml \
     --seed  $data/seeds/sn.tsv \
     --nodes $data/graph/nodes.tsv
